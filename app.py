2646fbb59522d62d428763060385da757ded4db3
Ebrahim Byagowi <ebrahim@gnu.org>
2016-02-12 22:27:32 +0000

    Use even a simpler URL

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 
 import json
-import urllib.request
 import urllib.parse
 import re
 import calverter
+import requests
 from bs4 import BeautifulSoup
 from flask import Flask, request, Response
 
 app = Flask(__name__)
 
 
-@app.route('/uploadhelper-ir/tasnimgallery/<path:url>')
+@app.route('/uploadhelper-ir/<path:url>')
 def tasnimgallery(url):
     try:
-        url = 'http://www.tasnimnews.com/' + url
-        page = urllib.request.urlopen(
-            urllib.parse.quote(url, safe='~@#$&()*!+=:;,.?/\''))
-        soup = BeautifulSoup(page.read(), 'html.parser')
+        if url.find('http://tasnimnews.com/') != 0:
+            raise Exception('Not supported link')
+
+        soup = BeautifulSoup(requests.get(url).text, 'html.parser')
         article = soup.select_one('body.photos article.media')
         images = map(lambda x: {
             'link': x['href'],
@@ -28,7 +28,8 @@ def tasnimgallery(url):
             'reporter': article.select_one('h4.reporter').text.strip(),
             'time': parsedate(article.select_one('time').text.strip()),
             'lead': article.select_one('h3.lead').text.strip(),
-            'result': list(images)
+            'images': list(images),
+            'url': url
         }
     except Exception as e:
         result = {"error": str(e)}
@@ -70,9 +71,9 @@ months = {
 distance = ord('۰') - ord('0')
 
 
-def parsedate(str):
-    str = re.sub(r'[۰-۹]', lambda x: chr(ord(x.group(0)) - distance), str)
-    m = re.match(r'(\d\d?) ([^ ]*) (\d{4}) - (\d\d?):(\d\d?)', str)
+def parsedate(date):
+    date = re.sub(r'[۰-۹]', lambda x: chr(ord(x.group(0)) - distance), date)
+    m = re.match(r'(\d\d?) ([^ ]*) (\d{4}) - (\d\d?):(\d\d?)', date)
     jd = cal.jalali_to_jd(int(m.group(3)), months[m.group(2)], int(m.group(1)))
     greg = cal.jd_to_gregorian(jd)
     return '%d-%d-%d' % greg + ' %s:%s' % (m.group(4), m.group(5))
