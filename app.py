6a444461e929c974072d77977a251916f2d1aab3
Ebrahim Byagowi <ebrahim@gnu.org>
2016-02-12 17:54:10 +0000

    Parse tasnimnews date

--- app.py
+++ app.py
@@ -3,6 +3,8 @@
 import json
 import urllib.request
 import urllib.parse
+import re
+import calverter
 from bs4 import BeautifulSoup
 from flask import Flask, request, Response
 
@@ -24,7 +26,7 @@ def tasnimgallery(url):
         result = {
             'title': article.select_one('h1.title').text.strip(),
             'reporter': article.select_one('h4.reporter').text.strip(),
-            'time': article.select_one('time').text.strip(),
+            'time': parsedate(article.select_one('time').text.strip()),
             'lead': article.select_one('h3.lead').text.strip(),
             'result': list(images)
         }
@@ -49,3 +51,28 @@ def tasnimcrop(url):
 
 if __name__ == '__main__':
     app.run()
+
+cal = calverter.Calverter()
+months = {
+  "فروردین": 1,
+  "اردیبهشت": 2,
+  "خرداد": 3,
+  "تیر": 4,
+  "مرداد": 5,
+  "شهریور": 6,
+  "مهر": 7,
+  "آبان": 8,
+  "آذر": 9,
+  "دی": 10,
+  "بهمن": 11,
+  "اسفند": 12
+}
+distance = ord('۰') - ord('0')
+
+
+def parsedate(str):
+    str = re.sub(r'[۰-۹]', lambda x: chr(ord(x.group(0)) - distance), str)
+    m = re.match(r'(\d\d?) ([^ ]*) (\d{4}) - (\d\d?):(\d\d?)', str)
+    jd = cal.jalali_to_jd(int(m.group(3)), months[m.group(2)], int(m.group(1)))
+    greg = cal.jd_to_gregorian(jd)
+    return '%d-%d-%d' % greg + ' %s:%s' % (m.group(4), m.group(5))
